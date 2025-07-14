6f532d0b6d557681c61526a6991ec9ee95668da1
Doug Parrish <115043460+dougdet@users.noreply.github.com>
2025-07-14 17:00:41 -0600

    Update dp_quik.py

--- aie02-airflow-dags/dp_quik.py
+++ aie02-airflow-dags/dp_quik.py
@@ -39,7 +39,7 @@ with DAG(
         'av_conn_id': Param("ce-dougdet-avdata", type="string"),
         'weave_conn_id': Param("ce-dougdet-weaviate", type="string"),
         's3_bucket': Param("ce-dougdet-pcaiexer", type="string"),
-        's3_prefix': Param("/", type="string"),
+        's3_prefix': Param(None, type="string"),
         'shared_vol_base': Param("/mnt/shared/", type="string"),
         'dnld_path': Param("dougdet", type="string"),
         'dnld_dir': Param("from_airflow", type="string")
