8981e125d4a235d0327f6cc16f82ed8d1973c77a
fcaliva <caliva.francesco@gmail.com>
2025-10-27 11:44:23 -0700

    fixed plot_utils function call in blood vessels analysis demo

--- blood-vessel-geometry-analysis-and-reconstruction/utils/plot_utils.py
+++ blood-vessel-geometry-analysis-and-reconstruction/utils/plot_utils.py
@@ -6,6 +6,14 @@ import os
 import plotly.graph_objects as go
 from typing import Dict
 
+from matplotlib.widgets import Slider
+from matplotlib.colors import ListedColormap, BoundaryNorm
+import nibabel as nib
+from pathlib import Path
+from IPython import display
+import time
+import plotly.graph_objects as go
+
 
 def mesh_to_plotly(
     mesh_data: Dict[str, np.ndarray], color="gray", name="Mesh", showlegend=True
@@ -213,3 +221,181 @@ def create_flat_disc(center, radius, normal, resolution=60):
     mesh.translate(center)
 
     return mesh
+
+
+# --- Utility functions to visualize the segmentation overlayed to the acquisition scan ---
+def visualize_slice(image, mask=None):
+    """
+    Visualize slice.
+
+    Args:
+        image: 2D np.array to visualize
+        mask: Optional 2D np.array to overlay
+    """
+    plt.imshow(image, cmap=plt.cm.gray)
+    if mask is not None:
+        plt.imshow(
+            np.ma.masked_where(mask == 0, mask),
+            cmap="jet",
+            alpha=0.5,
+            interpolation="none",
+        )
+
+
+def visualize_scan(
+    image, mask=None, interval=5, pause_time=0.1, show_slices=None, slice_dim="first"
+):
+    """
+    Visualize volume.
+
+    Args:
+        image: 3D np.array to visualize
+        mask: Optional 3D np.array to overlay
+        interval: int, interval between slices to be visualized
+        pause_time=0.1: float, pause between slice visualization
+        show_slices=None: Optional[List[int]]. If None, shows all slices at the interval rate, else shows the specified slices
+        slice_dim="first", whether slices are in the 'first' or 'last' dimension of the volume.
+
+    """
+    if slice_dim == "first":
+        show_slices = show_slices or range(0, image.shape[0], interval)
+    elif slice_dim == "last":
+        show_slices = show_slices or range(0, image.shape[-1], interval)
+    else:
+        raise ValueError("slice axis must be first or last. Used ", slice_axis)
+
+    for n in show_slices:
+        if slice_dim == "first":
+            visualize_slice(image[n] * 1000, mask[n] if mask is not None else None)
+            # plt.imshow(image[n] * 1000, cmap=plt.cm.gray)
+            # if mask is not None:
+            # plt.imshow(np.ma.masked_where(mask[n]==0, mask[n]), cmap="jet", alpha=0.5, interpolation="none")
+        elif slice_dim == "last":
+            visualize_slice(
+                image[..., n] * 1000, mask[..., n] if mask is not None else None
+            )
+            # plt.imshow(image[...,n] * 1000, cmap=plt.cm.gray)
+            # if mask is not None:
+            #     plt.imshow(np.ma.masked_where(mask[...,n]==0, mask[...,n]), cmap="jet", alpha=0.5, interpolation="none")
+
+        plt.title(f"Slice {n}")
+        display.display(plt.gcf())
+        display.clear_output(wait=True)
+        time.sleep(pause_time)
+
+
+def show_scan_with_slider(
+    scan_path=None,
+    segmentation_path=None,
+    ct_volume=None,
+    ct_mask=None,
+    segmentation_dict=None,
+):
+    # Load image and mask
+    if ct_volume is None:
+        assert (
+            scan_path is not None
+        ), f"scan_path cannot be None, please specify it or specify ct_volume"
+        volume = np.flipud(np.transpose(nib.load(scan_path).get_fdata(), (1, 0, 2)))
+    else:
+        volume = ct_volume
+    if ct_mask is None:
+        assert (
+            segmentation_path is not None
+        ), f"segmentation_path cannot be None, please specify it or specify mask_volume"
+        mask = np.flipud(
+            np.transpose(np.load(segmentation_path)["new_seg_scan"], (1, 0, 2))
+        )
+    else:
+        mask = ct_mask
+
+    init_slice = 0  # volume.shape[2]-1# // 2
+
+    # Class labels and colors
+
+    label_to_name = {
+        value: key for key, value in segmentation_dict.items() if value not in [0, "0"]
+    }
+
+    n_classes = len(label_to_name) + 1
+
+    # Choose a base colormap (tab10 is good for up to 10 distinct colors)
+    if n_classes == 4:
+        colors = ["white", "red", "blue", "green"]
+    elif 3 < n_classes <= 10:
+        base_cmap = plt.cm.get_cmap("tab10", n_classes)  # Or 'tab20'
+        colors = [base_cmap(i) for i in range(n_classes)]
+    elif 11 < n_classes <= 20:
+        base_cmap = plt.cm.get_cmap("tab20", n_classes)
+        colors = [base_cmap(i) for i in range(n_classes)]
+    else:
+        base_cmap = plt.cm.get_cmap("nipy_spectral", n_classes)
+        colors = [base_cmap(i) for i in range(n_classes)]
+
+    # Make a ListedColormap
+    cmap = ListedColormap(colors)
+    norm = BoundaryNorm(np.arange(-0.5, n_classes + 0.5), cmap.N)
+
+    # Create figure and axis
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.3)
+
+    c_ct = volume[:, :, init_slice]
+    c_mask = mask[:, :, init_slice]
+
+    img_display = ax.imshow(c_ct, cmap="gray")
+    mask_display = ax.imshow(
+        np.ma.masked_where(c_mask == 0, c_mask), cmap=cmap, norm=norm, alpha=0.5
+    )
+
+    ax.set_title(f"Slice {init_slice}")
+
+    # Add a slider
+    slider_ax = plt.axes([0.1, 0.15, 0.8, 0.05])
+    slider = Slider(
+        slider_ax, "Slice", 0, volume.shape[2] - 1, valinit=init_slice, valfmt="%0.0f"
+    )
+
+    def update(val):
+        slice_index = int(slider.val)
+        c_ct = volume[:, :, slice_index]
+        c_mask = mask[:, :, slice_index]
+
+        img_display.set_data(c_ct)
+        mask_display.set_data(np.ma.masked_where(c_mask == 0, c_mask))
+
+        ax.set_title(f"Slice {slice_index}")
+        fig.canvas.draw_idle()
+
+    slider.on_changed(update)
+    plt.show()
+
+
+def render_point_cloud(points, dest_dir=Path("renderings"), file_name="rendering"):
+    # Create a scatter3d plot
+    x = points[:, 0]
+    y = points[:, 1]
+    z = points[:, 2]
+    fig = go.Figure(
+        data=go.Scatter3d(
+            x=x,
+            y=y,
+            z=z,
+            mode="markers",
+            marker=dict(size=2, color=z, colorscale="Viridis"),
+        )
+    )
+    # Customize layout
+    fig.update_layout(
+        title=f"{Path(dest_dir).name} - {file_name}",
+        scene=dict(
+            xaxis_title="X", yaxis_title="Y", zaxis_title="Z", aspectmode="data"
+        ),
+        margin=dict(l=0, r=0, b=0, t=30),
+    )
+
+    # Save to HTML (fully interactive and offline)
+    os.makedirs(dest_dir, exist_ok=True)
+    fig.write_html(
+        Path(dest_dir) / Path(file_name + ".html"), auto_open=True
+    )  # auto_open opens it in browser
